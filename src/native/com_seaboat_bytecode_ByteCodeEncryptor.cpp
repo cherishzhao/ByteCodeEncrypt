74a78bebc288a530c6cea9a0521ecab12050bf25
cherishzhao <chenhaixuan@126.com>
2020-06-17 15:48:35 +0800

    Update com_seaboat_bytecode_ByteCodeEncryptor.cpp

--- src/native/com_seaboat_bytecode_ByteCodeEncryptor.cpp
+++ src/native/com_seaboat_bytecode_ByteCodeEncryptor.cpp
@@ -57,7 +57,7 @@ void JNICALL ClassDecryptHook(
 
 	unsigned char* _data = *new_class_data;
 
-	if (name&&strncmp(name, "com/seaboat/", 12) == 0) {
+	if (name&&strncmp(name, "com/mantis/", 12) == 0) {
 		for (int i = 0; i < class_data_len; i++)
 		{
 			_data[i] = class_data[i];
